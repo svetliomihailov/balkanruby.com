670a2a1b0a556c6002b7e95601d6ac5c0d000a58
Genadi Samokovarov <gsamokovarov@gmail.com>
2018-02-05 17:32:22 +0200

    Fix typos in the footer

--- layouts/Footer.js
+++ layouts/Footer.js
@@ -2,14 +2,14 @@ export default () => (
   <div className="site-footer purple-bg inverted-text-bg">
     <div className="container">
       <div className="footer-cols">
-        <div className="left-col">&copy; 2017. Neuevents</div>
+        <div className="left-col">&copy; 2017-2018. Neuvents</div>
         <div className="center-col">
           <a href="http://berlincodeofconduct.org/">Code of conduct</a>
           <a href="/sponsors">Partnership opportunities</a>
           <a href="mailto:hi@balkanruby.com?subject=Get in touch">Contact us</a>
         </div>
         <div className="right-col">
-          launched by{' '}
+          launched by {' '}
           <a href="http://raketadesign.com" target="_blank">
             raketa
           </a>
