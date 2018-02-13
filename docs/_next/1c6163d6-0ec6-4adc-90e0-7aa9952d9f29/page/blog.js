64ada65599ebe74e91ac1b436d079a83e12ce9ca
Vestimir Markov <me@vestimir.com>
2018-02-13 09:51:10 +0100

    Update packages

---
+++ docs/_next/1c6163d6-0ec6-4adc-90e0-7aa9952d9f29/page/blog.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/blog', function() {
+            var comp = module.exports=webpackJsonp([16],{236:function(e,t,u){e.exports=u(237)},237:function(e,t,u){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var n=u(0),l=function(e){return e&&e.__esModule?e:{default:e}}(n);t.default=function(){return l.default.createElement("div",null,l.default.createElement("h1",null,"Blog here"))}}},[236]);
+            return { page: comp.default }
+          })
+        
